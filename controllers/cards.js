2639277b189eeb65e269ecb1dd9ddba8cd9fb68e
Elena Silina <silina.prQgmail.com>
2020-04-12 14:24:26 -0700

    added card not found handler

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,5 +1,19 @@
 const Card = require('../models/card');
 
+class CardNotFoundError extends Error {
+  constructor() {
+    super('Card not found');
+  }
+}
+
+function handleCardNotFound(err, res) {
+  let status = 500;
+  if (err instanceof CardNotFoundError) {
+    status = 404;
+  }
+  res.status(status).send({ message: err.message });
+}
+
 module.exports.getCards = (req, res) => {
   Card.find({})
     .populate(['owner', 'likes'])
@@ -23,32 +37,32 @@ module.exports.createCard = (req, res) => {
 };
 
 module.exports.deleteCard = (req, res) => {
-  Card.findById(req.params.cardId)
+  Card.findById(req.params.cardId).orFail(() => new CardNotFoundError())
     .then((card) => {
-      if (card.owner.toString() === req.user._id) {
+      if (card.owner.equals(req.user._id)) {
         Card.findByIdAndRemove(req.params.cardId)
-          .then((item) => res.status(202).send({ data: item }))
+          .then((item) => res.status(200).send({ data: item }))
           .catch((err) => res.status(500).send({ message: err.message }));
       } else {
-        res.status(404).send({ message: 'This card belongs to another user' });
+        res.status(403).send({ message: 'This card belongs to another user' });
       }
     })
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => handleCardNotFound(err, res));
 };
 
 module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $addToSet: { likes: req.user._id } },
   { new: true },
-)
+).orFail(() => new CardNotFoundError())
   .populate(['owner', 'likes'])
   .then((card) => res.status(200).send({ data: card }))
-  .catch((err) => res.status(500).send({ message: err.message }));
+  .catch((err) => handleCardNotFound(err, res));
 
 module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $pull: { likes: req.user._id } },
   { new: true },
-)
+).orFail(() => new CardNotFoundError())
   .then((card) => res.status(200).send({ data: card }))
-  .catch((err) => res.status(500).send({ message: err.message }));
+  .catch((err) => handleCardNotFound(err, res));
